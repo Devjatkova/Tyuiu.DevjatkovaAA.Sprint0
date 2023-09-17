71663670735e3ded36fc60321c262b0c38097a1f
Tyuiu.DevjatkovaAA@outlook.com <annastaia1414@gmail.com>
2023-09-17 14:39:08 +0500

    Task7

---
+++ Tyuiu.DevjatkovaAA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.DevjatkovaAA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        //Пример циклической структуры (цикл с параметрами) for
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resulArray = new int[5];
+            for (var i = 0; i < resulArray.Length; i++)
+            {
+                resulArray[i] = numOne[i] + numTwo[i];
+            }
+            return resulArray;
+        }
+    }
+}
---
+++ Tyuiu.DevjatkovaAA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DevjatkovaAA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DevjatkovaAA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5ded293-4bf5-4fe0-ae7a-18adb9ee49ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
